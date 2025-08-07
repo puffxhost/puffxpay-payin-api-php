7f96ae84f9c40f24916f0a8eb5e65dc2f2cd3af3
Puffx Host <nitinmehta368@gmail.com>
2025-08-07 11:46:34 +0530

    Delete php directory

--- php/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-// PayIN Gateway Config
-define('PAYIN_API_TOKEN', 'YOUR_USER_TOKEN'); // Replace with real token
-define('API_CREATE_ORDER_URL', 'https://pay.x-server.in/api/create-order');
-define('API_CHECK_STATUS_URL', 'https://pay.x-server.in/api/check-order-status');
